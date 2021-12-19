a600b459d52891d644dfaf5f0a5404ee7c5dee07
moukrime <moukrimeabdelhaq@gmail.com>
2021-12-19 21:52:37 +0100

    add react-native-vector-icons

--- template/src/screens/home/index.tsx
+++ template/src/screens/home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 export default function Home() {
   return (
     <View
@@ -9,6 +11,7 @@ export default function Home() {
         alignItems: 'center',
       }}>
       <Text>Home</Text>
+      <Icon name="rocket" size={30} color="#900" />
     </View>
   );
 }
